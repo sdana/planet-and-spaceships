c2bf760d7660e94912e69934fa25097ebdc7f032
Seth Dana <seth.dana@gmail.com>
2018-08-22 18:54:43 -0500

    Not perfect, but working

--- Program.cs
+++ Program.cs
@@ -17,10 +17,73 @@ namespace PlanetAndSpaceship
             planetList.Add("Pluto");
             List<string> rockyPlanets = new List<string>();
             rockyPlanets = planetList.GetRange(0,4);
-            foreach (string planet in rockyPlanets)
-            {
-                System.Console.WriteLine(planet);
-            }
+            planetList.Remove("Pluto");
+
+            List<Dictionary<string, string>> visitedPlanets = new List<Dictionary<string, string>>();
+                Dictionary<string, string> Mercury = new Dictionary<string, string>() {
+                    {"Mariner 10", "Mercury"}, 
+                    {"MESSENGER", "Mercury"},
+                };
+                
+                Dictionary<string, string> Venus = new Dictionary<string, string>() {
+                    {"Mariner 2", "Venus"}, 
+                    {"Mariner 5", "Venus"},
+                };
+
+                Dictionary<string, string> Earth = new Dictionary<string, string>(){
+                    {"Pioneer 10", "Earth"}, 
+                    {"Pioneer 11", "Earth"}, 
+                    {"Voyager", "Earth"},
+                };
+
+                Dictionary<string, string> Mars = new Dictionary<string, string>() {
+                    {"Mariner 3", "Mars"},
+                    {"Mariner 4", "Mars"}, 
+                    {"Mariner 6", "Mars"},
+                    {"Mariner 7", "Mars"},
+                };
+
+                Dictionary<string, string> Jupiter = new Dictionary<string, string>(){
+                    {"Pioneer 10","Jupiter"}, 
+                    {"Pioneer 11", "Jupiter"}, 
+                    {"Gallileo","Jupiter"},
+                };
+
+                Dictionary<string, string> Saturn = new Dictionary<string, string>() {
+                    {"Cassini", "Saturn"},
+                };
+
+                Dictionary<string, string> Uranus = new Dictionary<string, string>() {
+                    {"Voyager 2", "Uranus"},
+                };
+
+                Dictionary<string, string> Neptune = new Dictionary<string, string>(){
+                    {"Voyager 2", "Neptune"}
+                };
+                visitedPlanets.Add(Mercury);
+                visitedPlanets.Add(Venus);
+                visitedPlanets.Add(Earth);
+                visitedPlanets.Add(Mars);
+                visitedPlanets.Add(Jupiter);
+                visitedPlanets.Add(Saturn);
+                visitedPlanets.Add(Uranus);
+                visitedPlanets.Add(Neptune);
+
+                foreach (string planet in planetList)
+                {
+                    System.Console.WriteLine($"Visitors to {planet}:");
+                    foreach (Dictionary<string, string> probeList in visitedPlanets)
+                    {
+                        foreach (KeyValuePair<string, string> probePlanet in probeList)
+                        {
+                           if (probePlanet.Value == planet)
+                           {
+                               System.Console.WriteLine(probePlanet.Key);
+                           }
+                        }
+                    }
+                }
+
         }
     }
 }
